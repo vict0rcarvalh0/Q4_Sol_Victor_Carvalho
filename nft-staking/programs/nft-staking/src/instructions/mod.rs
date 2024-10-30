9c580dc6f448eb62354caab8fd4625fc7bdc8798
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2024-10-29 21:34:31 -0300

    feat: nft staking project

---
+++ nft-staking/programs/nft-staking/src/instructions/mod.rs
@@ -0,0 +1,7 @@
+pub mod initialize_config;
+pub mod initialize_user_account;
+pub mod stake;
+
+pub use initialize_config::*;
+pub use initialize_user_account::*;
+pub use stake::*;
---
+++ nft-staking/programs/nft-staking/src/lib.rs
@@ -0,0 +1,29 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("HaYHTPna4YxqfvqmuMyW5hSPx8iaEKSbvgTKijJUyve7");
+
+#[program]
+pub mod nft_staking {
+    use super::*;
+
+    pub fn initialize_config(ctx: Context<InitializeConfig>, points_per_stake: u8, max_stake: u8, freeze_period: u32) -> Result<()> {
+        ctx.accounts.init_config(points_per_stake, max_stake, freeze_period, &ctx.bumps)
+    }
+
+    pub fn initialize_user(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.init_user(&ctx.bumps)
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        ctx.accounts.stake(&ctx.bumps)
+    }
+}
